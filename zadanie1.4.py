f610291240e0ec6268c0412397232be2eabd846f
Karol <abram86@wp.pl>
2026-05-07 19:20:04 +0200

    Zadanie 1.4

---
+++ zadanie1.4.py
@@ -0,0 +1,17 @@
+print("Zadanie 1.4\n")
+print("Nr1\n")
+for j in range(10):
+    if j%2 == 1:
+        print(" ",end="")
+    for i in range(10):       
+        print('* ',end="")
+    print()
+print()
+print("Nr2\n")
+for j in range(1,4):
+    for i in range(2):
+        print("*" * j * 2)
+print()
+print("Nr3\n")
+for i in range(6,0,-1):
+    print("*" * i)
