7a8f4743af30c505c6610861504f72b2ec564235
Karol <abram86@wp.pl>
2026-05-08 17:26:38 +0200

    Poprawka modyfied_chart_report

--- modyfied_chart_report.py
+++ modyfied_chart_report.py
@@ -25,7 +25,17 @@ quantity_8 = 0.22
 item_9 = "Listek miętowy"
 price_9 = 20
 quantity_9 = 0.2
-total = price_1 + price_2 + price_3 + price_4 + price_5 + price_6 + price_7 + price_8 + price_9
+total = (
+    price_1 * quantity_1 +
+    price_2 * quantity_2 +
+    price_3 * quantity_3 +
+    price_4 * quantity_4 +
+    price_5 * quantity_5 +
+    price_6 * quantity_6 +
+    price_7 * quantity_7 +
+    price_8 * quantity_8 +
+    price_9 * quantity_9
+)
 print (f'='*85)
 print (f"===================================Lista zakupów=====================================")
 print (f"{'Artykuł':<40}   \t{'Cena':>6}   *\t{'Ilość'}  =   {'Suma'}")
@@ -35,9 +45,9 @@ print (f"{item_3:<40}   \t{price_3:>6.2f}   *\t {quantity_3:.2f}  = {price_3*qua
 print (f"{item_4:<40}   \t{price_4:>6.2f}   *\t {quantity_4:.2f}  = {price_4*quantity_4:>6.2f} zł")
 print (f"{item_5:<40}   \t{price_5:>6.2f}   *\t {quantity_5:.2f}  = {price_5*quantity_5:>6.2f} zł")
 print (f"{item_6:<40}   \t{price_6:>6.2f}   *\t {quantity_6:.2f}  = {price_6*quantity_6:>6.2f} zł")
-print (f"{item_7:<40}   \t{price_7:>6.2f}   *\t {quantity_7:.2f}  = {price_1*quantity_7:>6.2f} zł")
-print (f"{item_8:<40}   \t{price_8:>6.2f}   *\t {quantity_8:.2f}  = {price_1*quantity_8:>6.2f} zł")
-print (f"{item_9:<40}   \t{price_9:>6.2f}   *\t {quantity_9:.2f}  = {price_1*quantity_9:>6.2f} zł")
+print (f"{item_7:<40}   \t{price_7:>6.2f}   *\t {quantity_7:.2f}  = {price_7*quantity_7:>6.2f} zł")
+print (f"{item_8:<40}   \t{price_8:>6.2f}   *\t {quantity_8:.2f}  = {price_8*quantity_8:>6.2f} zł")
+print (f"{item_9:<40}   \t{price_9:>6.2f}   *\t {quantity_9:.2f}  = {price_9*quantity_9:>6.2f} zł")
 print (f'='*85)
 print (f"====================================Podsumowanie=====================================")
 print (f"{'':<75}{'Razem':>6}")
