843dafaf936e8fa6199029c43fbfc8714eeb9f96
Karol <abram86@wp.pl>
2026-05-12 21:20:35 +0200

    Cwiczenia do krotki

---
+++ task_2.2.py
@@ -0,0 +1,9 @@
+greetings = "cześć, poskramiaczu lwów"
+print(dir(greetings))
+my_list = list()
+print(dir(my_list))
+
+days = ("pon", "wto", "śro")
+day_1,day_2,day_3 =days
+print(day_2)
+print(days[1])
---
+++ task_2.3.py
@@ -0,0 +1,9 @@
+direction_list = ["Pn","Pd"]
+direction_list.append("Wsch")
+print(direction_list)
+direction_list.append("Zach")
+print(direction_list)
+del direction_list[0:2]
+print(direction_list)
+direction_list.remove("Zach")
+print(direction_list)
