76b97301918fc07aa93721e1048675cdaa1e83be
Karol <abram86@wp.pl>
2026-05-14 19:33:03 +0200

    Cwiczenia modyfikacje kolekcji

--- task_2.3.py
+++ task_2.3.py
@@ -7,3 +7,28 @@ del direction_list[0:2]
 print(direction_list)
 direction_list.remove("Zach")
 print(direction_list)
+
+# lista 
+my_list = [3,6,17,4,0,-20,20,100]
+print(my_list)
+my_list.sort()
+print(my_list)
+my_list = my_list.sort()
+print(my_list)
+
+my_list = [3,6,17,4,0,-20,20,100]
+my_list = sorted(my_list)
+print(my_list)
+
+# update cwiczenie
+my_set = {'pon', 'wto', 'sro', 'pia', 'sob', 'nie'}
+my_set.update({"czw"})
+print(my_set)
+# 
+salads = {
+    "owocowa": ["ananas", "truskawka", "jagody"],
+    "moja_buraczana": ["buraki", "ser kozi", "rukola"],
+    "mamina": ["groszek", "kukurydza", "majonez", "ziemniaki"]
+}
+
+print(salads["owocowa"])
